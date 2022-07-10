7415b8293a7c92015bf4f709f9f623e87a4e01b2
Shane Jaroch <chown_tee@proton.me>
2022-07-10 16:15:38 -0400

    use correct url

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <p>Nutra is the best company ever</p>
-        <a className="app-link" href="https://nutra1.tk" target="_blank" rel="noopener noreferrer">
+        <a className="app-link" href="https://nutra.tk" target="_blank" rel="noopener noreferrer">
           Go to Nutra
         </a>
       </header>
