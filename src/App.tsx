72c3a429cdcd410045530b8b7eaa3ecac4a69ecb
KthProg <kthprog@gmail.com>
2022-07-10 11:53:54 -0400

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Nutra is the best company ever
+        </p>
+        <a
+          className="app-link"
+          href="https://nutra1.tk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to Nutra
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
