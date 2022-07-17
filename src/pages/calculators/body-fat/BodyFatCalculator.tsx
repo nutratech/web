819a0328b9a6b3e1bd7b033bd20102a577237d03
Shane Jaroch <chown_tee@proton.me>
2022-07-17 15:18:58 -0400

    disable rule, resolve prettier / ESLint conflicts

--- src/pages/calculators/body-fat/BodyFatCalculator.tsx
+++ src/pages/calculators/body-fat/BodyFatCalculator.tsx
@@ -13,48 +13,40 @@ function BodyFatCalculator(): JSX.Element {
 
   const debouncedFormData = useDebounce(bodyFatForm, 500);
 
-  const [selectedTestTypes, setSelectedTestTypes] = useState(
-    {
-      navy: true,
-      sevenSite: true,
-      threeSite: true,
-    } as {
-      [key in calculatorConstants.BodyFatTestKeyType]: boolean;
-    }
-  );
+  const [selectedTestTypes, setSelectedTestTypes] = useState({
+    navy: true,
+    sevenSite: true,
+    threeSite: true,
+  } as {
+    [key in calculatorConstants.BodyFatTestKeyType]: boolean;
+  });
 
-  useEffect(
-    () => {
-      // TODO: dispatch action to trigger API call instead of calling directly
-      void CalculatorService.calculateBodyFatPercentage(bodyFatForm).then((bodyFatResponse) => {
-        setBodyFatData(bodyFatResponse);
-      });
-    },
-    [debouncedFormData]
-  );
+  useEffect(() => {
+    // TODO: dispatch action to trigger API call instead of calling directly
+    void CalculatorService.calculateBodyFatPercentage(bodyFatForm).then((bodyFatResponse) => {
+      setBodyFatData(bodyFatResponse);
+    });
+  }, [debouncedFormData]);
 
-  /* eslint-disable @typescript-eslint/indent */
   const requiredFields: calculatorConstants.BodyFatFieldName[] = useMemo(
-    () => Object.keys(selectedTestTypes)
-      .reduce<calculatorConstants.BodyFatFieldName[]>((required, testType) => [
-        ...required,
-        ...(
-          selectedTestTypes[testType as calculatorConstants.BodyFatTestKeyType] ?
-            calculatorConstants.RequiredFields[testType as calculatorConstants.BodyFatTestKeyType] :
-            []
-        ),
-      ], []),
+    () =>
+      Object.keys(selectedTestTypes).reduce<calculatorConstants.BodyFatFieldName[]>(
+        (required, testType) => [
+          ...required,
+          ...(selectedTestTypes[testType as calculatorConstants.BodyFatTestKeyType]
+            ? calculatorConstants.RequiredFields[testType as calculatorConstants.BodyFatTestKeyType]
+            : []),
+        ],
+        []
+      ),
     [selectedTestTypes]
   );
-  /* eslint-enable @typescript-eslint/indent */
 
   const handleSubmit = (formEvent: React.FormEvent<HTMLFormElement>): void => {
     formEvent.preventDefault();
   };
 
-  const onTestTypeSelected = (
-    type: calculatorConstants.BodyFatTestKeyType
-  ): void => {
+  const onTestTypeSelected = (type: calculatorConstants.BodyFatTestKeyType): void => {
     setSelectedTestTypes({
       ...selectedTestTypes,
       [type]: !selectedTestTypes[type],
@@ -80,7 +72,7 @@ function BodyFatCalculator(): JSX.Element {
       <Col lg="12">
         <section id="body-fat-calculator">
           <pre>
-          Result:
+            Result:
             {JSON.stringify(bodyFatData, null, 2)}
           </pre>
           <Row>
@@ -93,92 +85,74 @@ function BodyFatCalculator(): JSX.Element {
                 <Row>
                   <Col lg="12">
                     <Form.Group>
-                      <Form.Label>
-                        Calculations
-                      </Form.Label>
-                      {
-                        calculatorConstants.BodyFatTestTypes.map((testType) => <Form.Check
+                      <Form.Label>Calculations</Form.Label>
+                      {calculatorConstants.BodyFatTestTypes.map((testType) => (
+                        <Form.Check
                           key={testType}
                           type="checkbox"
                           name="test-types"
-                          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                           label={calculatorConstants.BodyFatTestNames[testType]}
                           checked={selectedTestTypes[testType]}
-                          onChange={
-                            (): void => {
-                              onTestTypeSelected(testType);
-                            }
-                          }
-                        />)
-                      }
+                          onChange={(): void => {
+                            onTestTypeSelected(testType);
+                          }}
+                        />
+                      ))}
                     </Form.Group>
                   </Col>
                 </Row>
                 <Row>
-                  {
-                    calculatorConstants.BodyFatFieldNames
-                      .filter((fieldName) =>
-                        // eslint-disable-next-line implicit-arrow-linebreak
-                        calculatorConstants.BodyFatFieldOptions[fieldName].length > 0 &&
-                        requiredFields.includes(fieldName)
-                      // eslint-disable-next-line function-paren-newline
-                      ).map((fieldName) => <Col lg="6" key={fieldName}>
-                        <Form.Group>
-                          <Form.Label>
-                            {calculatorConstants.BodyFatFieldLabels[fieldName]}
-                          </Form.Label>
-                          <Form.Select
-                            name={fieldName}
-                            value={bodyFatForm[fieldName]}
-                            placeholder={calculatorConstants.BodyFatFieldLabels[fieldName]}
-                            onChange={(evt): void => {
-                              onInputChange(evt);
-                            }}
-                            aria-label={calculatorConstants.BodyFatFieldLabels[fieldName]}
-                          >
-                            <option>
-                              Select {calculatorConstants.BodyFatFieldLabels[fieldName]}
+                  {calculatorConstants.BodyFatFieldNames.filter(
+                    (fieldName) =>
+                      // prettier-ignore
+                      calculatorConstants.BodyFatFieldOptions[fieldName].length > 0
+                      && requiredFields.includes(fieldName)
+                  ).map((fieldName) => (
+                    <Col lg="6" key={fieldName}>
+                      <Form.Group>
+                        <Form.Label>{calculatorConstants.BodyFatFieldLabels[fieldName]}</Form.Label>
+                        <Form.Select
+                          name={fieldName}
+                          value={bodyFatForm[fieldName]}
+                          placeholder={calculatorConstants.BodyFatFieldLabels[fieldName]}
+                          onChange={(evt): void => {
+                            onInputChange(evt);
+                          }}
+                          aria-label={calculatorConstants.BodyFatFieldLabels[fieldName]}
+                        >
+                          <option>
+                            Select {calculatorConstants.BodyFatFieldLabels[fieldName]}
+                          </option>
+                          {calculatorConstants.BodyFatFieldOptions[fieldName].map((option) => (
+                            <option key={option.value} value={option.value}>
+                              {option.name}
                             </option>
-                            {
-                              calculatorConstants.BodyFatFieldOptions[fieldName].map((option) =>
-                                // eslint-disable-next-line implicit-arrow-linebreak
-                                <option
-                                  key={option.value}
-                                  value={option.value}
-                                >
-                                  {option.name}
-                                </option>
-                              // eslint-disable-next-line function-paren-newline
-                              )
-                            }
-                          </Form.Select>
-                        </Form.Group>
-                      </Col>)
-                  }
-                  {
-                    calculatorConstants.BodyFatFieldNames
-                      .filter((fieldName) =>
-                        // eslint-disable-next-line implicit-arrow-linebreak
-                        calculatorConstants.BodyFatFieldOptions[fieldName].length <= 0 &&
-                        requiredFields.includes(fieldName)
-                      // eslint-disable-next-line function-paren-newline
-                      ).map((fieldName) => <Col lg="6" key={fieldName}>
-                        <Form.Group>
-                          <Form.Label>
-                            {calculatorConstants.BodyFatFieldLabels[fieldName]}
-                          </Form.Label>
-                          <Form.Control
-                            type={calculatorConstants.BodyFatFieldTypes[fieldName]}
-                            name={fieldName}
-                            value={bodyFatForm[fieldName]}
-                            placeholder={calculatorConstants.BodyFatFieldLabels[fieldName]}
-                            onChange={(evt): void => {
-                              onInputChange(evt);
-                            }}
-                          />
-                        </Form.Group>
-                      </Col>)
-                  }
+                          ))}
+                        </Form.Select>
+                      </Form.Group>
+                    </Col>
+                  ))}
+                  {calculatorConstants.BodyFatFieldNames.filter(
+                    (fieldName) =>
+                      // prettier-ignore
+                      calculatorConstants.BodyFatFieldOptions[fieldName].length <= 0
+                      && requiredFields.includes(fieldName)
+                  ).map((fieldName) => (
+                    <Col lg="6" key={fieldName}>
+                      <Form.Group>
+                        <Form.Label>{calculatorConstants.BodyFatFieldLabels[fieldName]}</Form.Label>
+                        <Form.Control
+                          type={calculatorConstants.BodyFatFieldTypes[fieldName]}
+                          name={fieldName}
+                          value={bodyFatForm[fieldName]}
+                          placeholder={calculatorConstants.BodyFatFieldLabels[fieldName]}
+                          onChange={(evt): void => {
+                            onInputChange(evt);
+                          }}
+                        />
+                      </Form.Group>
+                    </Col>
+                  ))}
                 </Row>
                 <Row>
                   <Col lg="12">
