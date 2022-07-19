408c62e59c93edcf7f48d0ffbc299298d1e876e2
Shane Jaroch <chown_tee@proton.me>
2022-07-19 15:34:43 -0400

    wip api service refactor

--- src/services/ApiService.test.ts
+++ src/services/ApiService.test.ts
@@ -1,12 +1,15 @@
 import ApiService from "./ApiService";
 
 describe("Real HTTP calls", () => {
+  const apiService = new ApiService("https://dev.nutra.tk/api");
+  const bogusApiService = new ApiService("https://googlewoudlx34.wooweowodl");
+
   // prettier-ignore
   test(
     "Real 200 HTTP call to ntserv [JSON res] yields deserializable response",
     async () => {
-      const res = await ApiService.call(new Request(
-        "https://dev.nutra.tk/api/pg/version",
+      const res = await apiService.call(new Request(
+        `${apiService.baseUrl}/pg/version`,
         { method: "GET" }
       ));
 
@@ -21,8 +24,8 @@ describe("Real HTTP calls", () => {
   test(
     "Real 200 HTTP call to ntserv WITH body [JSON res] yields deserializable response [from JSON]",
     async () => {
-      const res = await ApiService.call(
-        new Request("https://dev.nutra.tk/api/calc/1rm", {
+      const res = await apiService.call(
+        new Request(`${apiService.baseUrl}/calc/1rm`, {
           method: "POST",
           body: JSON.stringify({
             reps: 12,
@@ -42,8 +45,8 @@ describe("Real HTTP calls", () => {
   test(
     "Real 401 HTTP call to ntserv [JSON res] yields deserializable response",
     async () => {
-      const res = await ApiService.call(new Request(
-        "https://dev.nutra.tk/api/email/change",
+      const res = await apiService.call(new Request(
+        `${apiService.baseUrl}/email/change`,
         { method: "GET" }
       ));
 
@@ -58,8 +61,8 @@ describe("Real HTTP calls", () => {
   test(
     "Real 405 HTTP call to ntserv [TEXT res] yields deserializable response",
     async () => {
-      const res = await ApiService.call(new Request(
-        "https://dev.nutra.tk/api/pg/version",
+      const res = await apiService.call(new Request(
+        `${apiService.baseUrl}/pg/version`,
         { method: "POST" }
       ));
 
@@ -67,38 +70,38 @@ describe("Real HTTP calls", () => {
 
       const result = await res.json() as { data: Record<string, unknown> };
       expect(Object.keys(result)).toContain("data");
-      expect(Object.keys(result.data)).toContain("err_msg");
-      // TODO: resolve TS2339: Property 'err_msg' does not exist on type 'never'.
-      //  maybe it needs a special class e.g. ServerJsonResponse(code, Optional[err_msg], ...)
+      expect(Object.keys(result.data)).toContain("errMsg");
+      // TODO: resolve TS2339: Property 'errMsg' does not exist on type 'never'.
+      //  maybe it needs a special class e.g. ServerJsonResponse(code, Optional[errMsg], ...)
       // TODO: why doesn't "make lint" show this error, but WebStorm does?
-      expect(result.data.err_msg).toContain("Method Not Allowed");
+      expect(result.data.errMsg).toContain("Method Not Allowed");
     }
   );
 
   // prettier-ignore
   test(
-    "Real 200 HTTP call to ntserv [HTML res] yields deserializable response w/ err_msg prop",
+    "Real 200 HTTP call to ntserv [HTML res] yields deserializable response w/ errMsg prop",
     async () => {
-      const res = await ApiService.call(new Request("https://dev.nutra.tk/api/nutrients/html"));
+      const res = await apiService.call(new Request(`${apiService.baseUrl}/nutrients/html`));
 
       expect(res.status).toEqual(200);
 
       const result = await res.json() as { data: Record<string, unknown> };
       expect(Object.keys(result)).toContain("data");
-      expect(Object.keys(result.data)).toContain("err_msg");
-      expect(result.data.err_msg).toBeTruthy();
+      expect(Object.keys(result.data)).toContain("errMsg");
+      expect(result.data.errMsg).toBeTruthy();
     }
   );
 
   jest.setTimeout(15000);
   // prettier-ignore
   test(
-    "Real ECONNREFUSED call [ERROR res] to unreachable website yields err_msg prop",
+    "Real ECONNREFUSED call [ERROR res] to unreachable website yields errMsg prop",
     async () => {
-      const res = await ApiService.call(new Request("https://googlewoudlx34.wooweowodl"));
+      const res = await bogusApiService.call(new Request(bogusApiService.baseUrl));
 
       const result = await res.json() as { data: Record<string, unknown> };
-      expect(Object.keys(result.data)).toContain("err_msg");
+      expect(Object.keys(result.data)).toContain("errMsg");
     }
   );
 });
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,32 +1,38 @@
-async function call(dict: Request): Promise<Response> {
-  // prettier-ignore
-  return fetch(dict)
-    .then(async (response: Response) => {
-      console.debug(`Got response: ${JSON.stringify(response)}`);
+export default class ApiService {
+  public baseUrl: string;
 
-      // Handle Text (non-JSON) Content-Type
-      if (response.headers.get("content-type")?.startsWith("text/")) {
-        const bodyText = await response.text();
-        return new Response(JSON.stringify({
-          code: response.status,
-          data: { err_msg: bodyText },
-        }), {
-          status: response.status, statusText: bodyText,
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  async call(request: Request): Promise<Response> {
+    // prettier-ignore
+    return fetch(request)
+      .then(async (response: Response) => {
+        console.debug(`Got response: ${JSON.stringify(response)}`);
+
+        // Handle Text (non-JSON) Content-Type
+        if (response.headers.get("content-type")?.startsWith("text/")) {
+          const bodyText = await response.text();
+          return new Response(JSON.stringify({
+            code: response.status,
+            data: { errMsg: bodyText },
+          }), {
+            status: response.status, statusText: bodyText,
+          });
+        }
+        // TODO: try to parse the .json() or .text()
+
+        // return default
+        return response;
+      })
+      // Bundle the error (with message) as a new Response()
+      .catch((err: Error) => {
+        // TODO: is this proper? 418?
+        console.warn("ERROR: General API error. Not connected?");
+        return new Response(JSON.stringify({ code: 418, data: { errMsg: err.message } }), {
+          status: 418, statusText: err.message,
         });
-      }
-
-      // return default
-      return response;
-    })
-    // Bundle the error (with message) as a new Response()
-    .catch((err: Error) => {
-      console.warn("ERROR: General API error. Not connected?");
-      return new Response(JSON.stringify({ code: 418, data: { err_msg: err.message } }), {
-        status: 418, statusText: err.message,
       });
-    });
+  }
 }
-
-export default {
-  call,
-};
