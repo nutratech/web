40463610a0dd09ef99a72bcc44f7e1fc56999fca
Shane Jaroch <chown_tee@proton.me>
2026-01-27 18:19:12 -0500

    fix name issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@tg-svelte/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
